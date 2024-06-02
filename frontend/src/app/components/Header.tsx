f0aafbb0d4f1bbcd3180d8a5db60ad477dd899ff
armandyblakstvedt <89578819+armandyblakstvedt@users.noreply.github.com>
2024-06-02 12:19:55 +0200

    feat: add link to github repo

--- frontend/src/app/components/Header.tsx
+++ frontend/src/app/components/Header.tsx
@@ -1,11 +1,14 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react';
 
 export default function Header() {
   return (
     <div className=' flex flex-row items-center gap-4 '>
       <h1 className=' flex text-xl'>Kesameter</h1>
-      <Image src='/github-mark-white.svg' alt='Kesameter' width={30} height={30} />
+      <Link href='https://github.com/armandyblakstvedt/kesameter' target='_blank'>
+        <Image src='/github-mark-white.svg' alt='Kesameter' width={30} height={30} />
+      </Link>
     </div>
   );
 }
